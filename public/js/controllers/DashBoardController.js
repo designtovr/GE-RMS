55b4d065dc68c661e367a806cd3c44faa356cb4a
RupanRC <erupanc@gmail.com>
2019-11-25 00:10:03 +0530

    Dashboard js update

--- public/js/controllers/DashBoardController.js
+++ public/js/controllers/DashBoardController.js
@@ -1,6 +1,109 @@
 app.controller('DashBoardController', ['$scope', '$http', 'Notification' , '$location' , '$window' , '$interval', function($scope, $http ,  Notification , $location , $window, $interval)
 {
     $scope.dashboardvalues = {};
+    var warrantydata  = {
+        labels: ["PX40", "C264" , "Agile" ,"Conventional" ],
+        datasets: [
+            {
+                label: "Time Exceeded Relays",
+                data: [90, 59 ,90, 59],
+                stack: 'Stack 0',
+                borderColor: "rgba(0,0,0,0.09)",
+                borderWidth: "0",
+                backgroundColor: "rgba(255,0,0,0.4)",
+                fontFamily: "Poppins"
+            },
+            {
+                label: "Total",
+                data: [28, 48,90, 59],
+                stack: 'Stack 0',
+                borderColor: "rgba(0, 123, 255, 0.9)",
+
+                borderWidth: "0",
+                backgroundColor: "rgba(0, 123, 255, 0.5)",
+                fontFamily: "Poppins"
+            }
+
+
+        ]
+    }
+    
+    var ctx = document.getElementById("Warranty");
+    if (ctx) {
+        ctx.height = 200;
+        var stackedBar = new Chart(ctx, {
+            type: 'horizontalBar',
+            data: warrantydata,
+            options: {
+                scales: {
+                    xAxes: [{
+                        stacked: true,
+                        gridLines: {
+                            display: false
+                        },
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                        gridLines: {
+                            display: true
+                        },
+                        ticks: {
+                            /*                  max: Math.max(...data.datasets[0].data) + 40,
+                                              display: false,*/
+                            beginAtZero: true
+                        }
+                    }]
+                },
+
+                "hover": {
+                    "animationDuration": 0
+                },
+
+
+
+                "animation": {
+                    "duration": 1,
+                    "onComplete": function () {
+                        var chartInstance = this.chart,
+                            ctx = chartInstance.ctx;
+
+                        ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                        ctx.textAlign = 'center';
+                        ctx.textBaseline = 'bottom';
+                        ctx.color = 'black';
+                        this.data.datasets.forEach(function (dataset, i) {
+                            var meta = chartInstance.controller.getDatasetMeta(i);
+                            meta.data.forEach(function (bar, index) {
+                                var data = dataset.data[index];
+                                if(i ==1)
+                                {
+                                    data += warrantydata.datasets[0].data[index];
+                                }
+                                /*     if(index % 2 != 0)
+                                     {
+                                         console.log(data);
+                                         data += dataset.data[index -1];
+                                     }*/
+                                ctx.fillText(data, bar._model.x - 15, bar._model.y + 7);
+                            });
+                        });
+                    }
+                }
+            },
+            legend: {
+                display: false,
+                position : 'bottom'
+            },
+
+            tooltips: {
+                "enabled": false
+            }
+        });
+
+    }
 
     $scope.GoToReceipt = function() {
         $window.location.href = url('/receipt');
@@ -173,6 +276,70 @@ app.controller('DashBoardController', ['$scope', '$http', 'Notification' , '$loc
                             }
 
                         });
+                        myChart.destroy();
+                        myChart = new Chart(ctx, {
+                            type: 'bar',
+                            defaultFontFamily: 'Poppins',
+                            data: data,
+                            options: {
+                                "hover": {
+                                    "animationDuration": 0
+                                },
+
+
+
+                                "animation": {
+                                    "duration": 1,
+                                    "onComplete": function () {
+                                        var chartInstance = this.chart,
+                                            ctx = chartInstance.ctx;
+
+                                        ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                                        ctx.textAlign = 'center';
+                                        ctx.textBaseline = 'bottom';
+
+                                        this.data.datasets.forEach(function (dataset, i) {
+                                            var meta = chartInstance.controller.getDatasetMeta(i);
+                                            meta.data.forEach(function (bar, index) {
+                                                var data = dataset.data[index];
+                                                ctx.fillText(data, bar._model.x + 10, bar._model.y + 5);
+                                            });
+                                        });
+                                    }
+                                }
+                            },
+                            legend: {
+                                display: false,
+                                position : 'bottom'
+                            },
+
+                            tooltips: {
+                                "enabled": false
+                            },
+                            scales: {
+                                yAxes: [{
+                                    display: true,
+                                    gridLines: {
+                                        display: true
+                                    },
+                                    ticks: {
+                                        max: Math.max(...data.datasets[0].data) + 10,
+                                        display: false,
+                                        beginAtZero: true
+                                    }
+                                }],
+                                xAxes: [{
+                                    gridLines: {
+                                        display: false
+                                    },
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            }
+
+                        });
+
                     }
 
 
@@ -186,7 +353,7 @@ app.controller('DashBoardController', ['$scope', '$http', 'Notification' , '$loc
                     var ctx = document.getElementById("MonthlyStatus");
                     if (ctx) {
                         ctx.height = 200;
-                        var myChart = new Chart(ctx, {
+                        var monthlyChart = new Chart(ctx, {
                             type: 'horizontalBar',
                             defaultFontFamily: 'Poppins',
                             data: {
@@ -281,7 +448,7 @@ app.controller('DashBoardController', ['$scope', '$http', 'Notification' , '$loc
                     var ctx = document.getElementById("doughChart");
                     if (ctx) {
                         ctx.height = 200;
-                        var myChart = new Chart(ctx, {
+                        var dh = new Chart(ctx, {
                             type: 'doughnut',
                             data: {
                                 datasets: [{
@@ -322,81 +489,9 @@ app.controller('DashBoardController', ['$scope', '$http', 'Notification' , '$loc
                 try {
 
                     //Warranty chart
-                    var ctx = document.getElementById("Warranty");
-                    if (ctx) {
-                        ctx.height = 200;
-                        var stackedBar = new Chart(ctx, {
-                            type: 'horizontalBar',
-                            data: warrantydata,
-                            options: {
-                                scales: {
-                                    xAxes: [{
-                                        stacked: true,
-                                        gridLines: {
-                                            display: false
-                                        },
-                                        ticks: {
-                                            beginAtZero: true
-                                        }
-                                    }],
-                                    yAxes: [{
-                                        stacked: true,
-                                        gridLines: {
-                                            display: true
-                                        },
-                                        ticks: {
-                          /*                  max: Math.max(...data.datasets[0].data) + 40,
-                                            display: false,*/
-                                            beginAtZero: true
-                                        }
-                                    }]
-                                },
 
-                                    "hover": {
-                                        "animationDuration": 0
-                                    },
+                        stackedBar.update();
 
-
-
-                                    "animation": {
-                                        "duration": 1,
-                                        "onComplete": function () {
-                                            var chartInstance = this.chart,
-                                                ctx = chartInstance.ctx;
-
-                                            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-                                            ctx.textAlign = 'center';
-                                            ctx.textBaseline = 'bottom';
-                                            ctx.color = 'black';
-                                            this.data.datasets.forEach(function (dataset, i) {
-                                                var meta = chartInstance.controller.getDatasetMeta(i);
-                                                meta.data.forEach(function (bar, index) {
-                                                    var data = dataset.data[index];
-                                                    if(i ==1)
-                                                    {
-                                                        data += warrantydata.datasets[0].data[index];
-                                                    }
-                                               /*     if(index % 2 != 0)
-                                                    {
-                                                        console.log(data);
-                                                        data += dataset.data[index -1];
-                                                    }*/
-                                                    ctx.fillText(data, bar._model.x - 15, bar._model.y + 7);
-                                                });
-                                            });
-                                        }
-                                    }
-                                },
-                                legend: {
-                                    display: false,
-                                    position : 'bottom'
-                                },
-
-                                tooltips: {
-                                    "enabled": false
-                                }
-                        });
-                    }
                 }
                 catch (error)
                 {
@@ -409,7 +504,7 @@ app.controller('DashBoardController', ['$scope', '$http', 'Notification' , '$loc
                     var ctx = document.getElementById("OutOfWarranty");
                     if (ctx) {
                         ctx.height = 200;
-                        var stackedBar = new Chart(ctx, {
+                        var war = new Chart(ctx, {
                             type: 'horizontalBar',
                             data: outofwarrantydata,
                             options: {
@@ -493,7 +588,7 @@ app.controller('DashBoardController', ['$scope', '$http', 'Notification' , '$loc
                     var ctx = document.getElementById("doughChart1");
                     if (ctx) {
                         ctx.height = 200;
-                        var myChart = new Chart(ctx, {
+                        var dc1 = new Chart(ctx, {
                             type: 'doughnut',
                             data: {
                                 datasets: [{
