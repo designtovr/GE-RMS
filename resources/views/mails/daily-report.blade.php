9203cf1655e51830be7a248eee60df68db71fde1
designtovr <designtovr@gmail.com>
2020-03-17 14:14:07 +0530

    DailyReport Update MAil

--- resources/views/mails/daily-report.blade.php
+++ resources/views/mails/daily-report.blade.php
@@ -5,18 +5,19 @@
     <style type="text/css">
         table, th, tr {
           border: 1px solid black;
-        }
-        ul#tablerowlist li {
+      }
+      ul#tablerowlist li {
           display:inline;
           width: 33%;
-        }
-    </style>
+      }
+  </style>
 </head>
 <body>
     <div class="main-content">
         <div class="section__content section__content--p30">
             <ul class="tablerowlist" id="tablerowlist">
                 <li>
+                    <h2>Received Relays</h2>
                     <table>
                         <thead>
                             <th>Family</th>
@@ -25,15 +26,18 @@
                         </thead>
                         <tbody>
                             @foreach ($received_relays as $relays)
-                                                        <td >{{$relays->type_name}}</td>
-                                                        <td >{{$relays->serial_no}}</td>
-                                                        <td >{{$relays->type_name}}</td>
-
+                            <tr>
+                                <td >{{$relays->type_name}}</td>
+                                <td >{{$relays->total}}</td>
+                                <td >{{$relays->cumulative}}</td>
+                            </tr>
                             @endforeach
                         </tbody>
                     </table>
                 </li>
                 <li>
+                    <h2>Received Relays</h2>
+
                     <table>
                         <thead>
                             <th>Family</th>
@@ -43,35 +47,40 @@
                             <th>Total</th>
                         </thead>
                         <tbody>
-                        <tbody>
+                            <tbody>
                               <tbody>
-                      @foreach ($total_relays_completed as $relays)
-                                                        <td >{{$relays->type_name}}</td>
-                                                        <td >{{$relays->repair}}</td>
-                                                        <td >{{$relays->test}}</td>
-                                                        <td >{{$relays->dispatch}}</td>
-                                                        <td >{{$relays->total}}</td>
+                                  @foreach ($total_relays_completed as $relays)
+                                  <tr>
+                                    <td >{{$relays->type_name}}</td>
+                                    <td >{{$relays->repair}}</td>
+                                    <td >{{$relays->test}}</td>
+                                    <td >{{$relays->dispatch}}</td>
+                                    <td >{{$relays->total}}</td>
+                                </tr>
 
-                            @endforeach
-                        </tbody>
-                    </table>
-                </li>
-                <li>
-                    <table>
-                        <thead>
-                            <th>Family</th>
-                            <th>Repair</th>
-                            <th>Test</th>
-                            <th>Dispatch</th>
-                            <th>Total</th>
-                        </thead>
-                        <tbody>
-                      @foreach ($total_relays_overdues as $relays)
-                                                        <td >{{$relays->type_name}}</td>
-                                                        <td >{{$relays->repair}}</td>
-                                                        <td >{{$relays->test}}</td>
-                                                        <td >{{$relays->dispatch}}</td>
-                                                        <td >{{$relays->total}}</td>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </li>
+                    <li>
+                        <h2>Received Relays</h2>
+                        <table>
+                            <thead>
+                                <th>Family</th>
+                                <th>Repair</th>
+                                <th>Test</th>
+                                <th>Dispatch</th>
+                                <th>Total</th>
+                            </thead>
+                            <tbody>
+                              @foreach ($total_relays_overdues as $relays)
+                              <tr>
+                                <td >{{$relays->type_name}}</td>
+                                <td >{{$relays->repair}}</td>
+                                <td >{{$relays->test}}</td>
+                                <td >{{$relays->dispatch}}</td>
+                                <td >{{$relays->total}}</td>
+                            </tr>
 
                             @endforeach
                         </tbody>
@@ -80,18 +89,27 @@
             </ul>
             <ul class="tablerowlist" id="tablerowlist">
                 <li>
+                    <h2>Received Relays</h2>
+
                     <table>
                         <thead>
                             <th>Family</th>
                             <th>Total</th>
                         </thead>
                         <tbody>
-                            <td></td>
-                            <td></td>
+                            @foreach ($total_chargeable as $relays)
+                            <tr>
+                                <td >{{$relays->type_name}}</td>
+                                <td >{{$relays->total}}</td>
+                            </tr>
+
+                            @endforeach
                         </tbody>
                     </table>
                 </li>
                 <li>
+                    <h2>Received Relays</h2>
+
                     <table>
                         <thead>
                             <th>Conventional</th>
@@ -102,43 +120,64 @@
                             <th>Total</th>
                         </thead>
                         <tbody>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tbody>
-                    </table>
-                </li>
-                <li>
-                    <table>
-                        <thead>
-                            <th>Family</th>
-                            <th>Overdues</th>
-                        </thead>
-                        <tbody>
-                            <td></td>
-                            <td></td>
-                        </tbody>
-                    </table>
-                </li>
-            </ul>
-            <ul class="tablerowlist" id="tablerowlist">
-                <li>
-                    <table>
-                        <thead>
-                            <th>Type / Category</th>
-                            <th>Days</th>
-                        </thead>
-                        <tbody>
-                            <td></td>
-                            <td></td>
-                        </tbody>
-                    </table>
-                </li>
-            </ul>
-        </div>
-    </div>
+                         @foreach ($total_completed as $relays)
+                         <tr>
+                             <td >{{$relays->CONVENTIONAL}}</td>
+                             <td >{{$relays->NUMERICAL}}</td>
+                             <td >{{$relays->MULTILIN}}</td>
+                             <td >{{$relays->REASON}}</td>
+                             <td >{{$relays->BOJ}}</td>
+                             <td >{{$relays->total}}</td>
+                         </tr>
+                         @endforeach
+
+                     </tbody>
+                 </table>
+             </li>
+             <li>
+                <h2>Received Relays</h2>
+
+                <table>
+                    <thead>
+                        <th>Family</th>
+                        <th>Overdues</th>
+                    </thead>
+                    <tbody>
+                      @foreach ($total_completed as $relays)
+                      <tr>
+                         <td >{{$relays->CONVENTIONAL}}</td>
+                         <td >{{$relays->NUMERICAL}}</td>
+                         <td >{{$relays->MULTILIN}}</td>
+                         <td >{{$relays->REASON}}</td>
+                         <td >{{$relays->BOJ}}</td>
+                         <td >{{$relays->total}}</td>
+                     </tr>
+                     @endforeach
+                 </tbody>
+             </table>
+         </li>
+     </ul>
+     <ul class="tablerowlist" id="tablerowlist">
+        <li>
+            <h2>Received Relays</h2>
+
+            <table>
+                <thead>
+                    <th>Type / Category</th>
+                    <th>Days</th>
+                </thead>
+                <tbody>
+                 @foreach ($repair_lead_time as $relays)
+                 <tr>
+                     <td >{{$relays->type_name}}</td>
+                     <td >{{$relays->average}}</td>
+                 </tr>
+                 @endforeach
+             </tbody>
+         </table>
+     </li>
+ </ul>
+</div>
+</div>
 </body>
 </html>
